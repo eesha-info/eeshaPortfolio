e07603a1c83aceda69e251c1db24fd756ff90268
MD Eesha <134606309+eesha-info@users.noreply.github.com>
2026-09-08 23:50:47 +0530

    Update data.ts
    updated cv

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -288,7 +288,7 @@ export const socialLinks = {
   phone: "+91-9463258394",
   location: "Hyderabad, Telangana (India)",
   resume:
-    "https://drive.google.com/file/d/1Nr8JtvrSJ7gYgOJ-NWTZ1eXoId62Xfmd/view?usp=sharing",
+    "https://drive.google.com/file/d/1rcSAGADfYysAC5f9qsdtSyMN7lfKYbpn/view?usp=sharing",
 };
 
 const careerStartDate = new Date(2021, 8); // September 2021
